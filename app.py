8ea077a662e78ca1bfaa3867e95f8c3ca1b7ec63
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2024-01-26 18:56:24 +0000

    Changed app.py second time

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
 file_location = getenv("MSG_FILE_LOCATION", "msg.txt")
 
 @app.route("/", methods=["GET", "POST"])
